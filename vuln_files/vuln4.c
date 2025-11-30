1b26d74f589f67544b0e996a3f8d81f338367548
NicHarrod <os22128@bristol.ac.uk>
2025-11-30 17:05:45 +0000

    ljhyfhkfjtdtd

--- vuln_files/vuln4.c
+++ vuln_files/vuln4.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-
-
 int main() {
     char input1;
 
-    printf("Input1");
-    scanf(" %c", &input1); // The space before %c tells scanf to ignore leading whitespace
+    printf("Input1: ");
+    scanf(" %c", &input1);
+
     char input2;
-    printf("Input2");
+    printf("Input2: ");
     scanf(" %c", &input2);
 
-    while ((getchar()) != '\n'); // Clear the input buffer
+    // Clear the input buffer without getchar()
+    scanf("%*[^\n]");
+    scanf("%*c");
 
-    printf("Input3");
+    printf("Input3: ");
     char buffer[10];
-    gets(buffer);
+    gets(buffer);   // unsafe, but kept because you requested it
+
     printf("You entered: %s\n", buffer);
 
     return 0;
